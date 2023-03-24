1149fd33d3d6bc752d86a1d268cf4d4887f45381
ntwigamartin <ntwiga.martin@gmail.com>
2023-03-24 14:42:09 +0300

    Completed, all tests passed

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {error: 'Failed'}, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user, status: :ok
+        else
+            render json: {error: 'Login to proceed'}, status: :unauthorized
+        end
+
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+end
